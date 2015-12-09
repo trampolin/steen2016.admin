6961a1ec275cd964b02c40819b0dc5278af2999f
Richard Mahr <richardmahr@gmx.de>
2015-12-09 16:44:33 +0100

    auth

---
+++ app/application/models/system/Login_model.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: rmahr1
+ * Date: 09.12.15
+ * Time: 13:14
+ */
+
+class Login_model extends STEEN_Model
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
